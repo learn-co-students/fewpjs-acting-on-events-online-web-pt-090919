9ee977d39c7e3101ed0bc07f2fc2740c9a2122e1
fasnimo <fasnimo@gmail.com>
2020-04-03 20:00:34 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
-// Your code here
+let dodger = getElementById('dodger');
+dodger.stlye.backgroungColor = '#FF69B4';
+dodger.style.left;
+dodger.style.bottom = '0px';
+
+
+function moveDodgerLeft() {
+    let leftNumber = dodger.style.left.replace('px', '');
+    let left = parseInt(leftNumber, 10);
+
+    if (left > 0) {
+        dodger.style.left = `${left - 1}px`;
+    };
+       document.addEventListener("keydown", function(e) {
+            if (e.key === "ArrowLeft") {
+                moveDodgerLeft();
+            }
+        });
+}
+
+function moveDodgerRight() {
+    let leftNumber = dodger.style.left.replace('px', '');
+    let left = parseInt(leftNumber, 10);
+
+    if (left >= 179) {
+        dodger.style.left = `${left + 1}px`;
+    };
+
+    document.addEventListener("keydown", function() {
+        if (e.key === "ArrowRight") {
+            moveDodgerRight()
+        }
+     });
+}
